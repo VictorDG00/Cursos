e6e30c8cfb241e4fbb6131a750b9a619848710b5
Victor D. G <Victord.goncalves@outlook.com>
2020-11-06 11:30:52 -0300

    ex067 feito

--- cursoEmVideo/Python/Mundo 2/Exercicios/Ex067.py
+++ cursoEmVideo/Python/Mundo 2/Exercicios/Ex067.py
@@ -1,3 +1,13 @@
 # faça um programa que mostre a tabuada de varios numeros
 # um de cada vez, para cada valor digitado
 # o programa sera interrompido quando for solicityado um numero negativo
+
+while True:
+    multiplicado = int(input('Digite um numero para ver sua tabuada: '))
+    for tab in range(1, 11):
+        print(f'{multiplicado}x{tab}={multiplicado * tab}')
+    continuacao = str(input('Quer ver outra tabuada? [S/N]'))
+    if continuacao in 'Nn':
+        break
+print('cabo nego')
+
