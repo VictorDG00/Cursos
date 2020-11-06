a5dba17571d5623f757b2a806a949bfafbe74b06
Victor D. G <Victord.goncalves@outlook.com>
2020-11-06 09:54:43 -0300

    fiz tudo errado, mas ta ai

--- cursoEmVideo/Python/Mundo 2/Exercicios/Ex062.py
+++ cursoEmVideo/Python/Mundo 2/Exercicios/Ex062.py
@@ -1,12 +1,25 @@
 # melhore o 61 perguntando até que termo o programa deve mostrar
-
-primeiroTerm = int(input('Digite o primeiro termo para uma PA: '))
+#primeiroTerm = int(input('Digite o primeiro termo para uma PA: '))
+#razaoPa = int(input('Digite a razão dessa mesma PA: '))
+#ultTermo = int(input('Até que termo você gostaria de ver? '))
+#calcTermo = primeiroTerm + (ultTermo - 1) * razaoPa
+#contador = primeiroTerm
+#while contador < calcTermo + razaoPa:
+    #print(f'{contador}', end=' > ')
+    #contador = contador + razaoPa
+#print('Fim')
+primeiroTerm = int(input('Digite um termo para uma PA: '))
 razaoPa = int(input('Digite a razão dessa mesma PA: '))
-ultTermo = int(input('Até que termo você gostaria de ver? '))
-calcTermo = primeiroTerm + (ultTermo - 1) * razaoPa
-contador = primeiroTerm
-
-while contador < calcTermo + razaoPa:
-    print(f'{contador}', end=' > ')
-    contador = contador + razaoPa
-print('Fim')
+contador = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while contador <= total:
+        print(primeiroTerm, end=' > ')
+        primeiroTerm += razaoPa
+        contador += 1
+    print('Para meu irmão')
+    mais = int(input('Quer ver mais quantos termos?'))
+print('Cabo nego')
+print(f'{total} temos solicitados')
