8c8e02c293c168cade54747d96b915b2f1ed2718
VictorDG00 <victord.goncalves@outlook.com>
2021-01-27 07:25:16 -0300

    Ex77 Finalizado

--- cursoEmVideo/Python/Mundo 3/Exercicios/Ex077.py
+++ cursoEmVideo/Python/Mundo 3/Exercicios/Ex077.py
@@ -1,2 +1,9 @@
-# uma tupla com varias palavras não use ascentos
+# uma tupla com varias palavras não use acentos
 # depois disso vc deve mostrar para cada palavra quais são as saus vogais
+
+palavra = ('Coletar', 'Varinha', 'Marciano', 'Piedade', 'Anestesia', 'Julgamento', 'Italiano', 'Transporte')
+for p in palavra:
+    print(F'\nNa palavra {p} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra.lower(), end=' ')
