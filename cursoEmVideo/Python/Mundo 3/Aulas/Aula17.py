be32ae39482b9c114a7b8a6f3f6be2c9611381e2
VictorDG00 <victord.goncalves@outlook.com>
2021-02-10 09:10:17 -0300

    aula 17

---
+++ cursoEmVideo/Python/Mundo 3/Aulas/Aula17.py
@@ -0,0 +1,8 @@
+# array
+# array tem estrutura = ['0','1','2'] | para adicionar usa "var.append('tres')" ou ".insert(possição,'0')"
+# para apagar é "del var[possição]"
+num = [2, 4, 6, 7]
+num[2] = 3
+num.append(7)
+
+print(num)
